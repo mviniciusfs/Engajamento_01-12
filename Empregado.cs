247577f85758274308beb3820a9bed031facb568
Marcos_Vinici10 <viniciusfigueiredo@ucl.br>
2020-12-02 00:29:04 +0000

    newv

--- Empregado.cs
+++ Empregado.cs
@@ -16,16 +16,45 @@ class Empregado
     return Sobrenome;
   }
 
+  public void Setsobrenome(string sn)
+  {
+    Sobrenome = sn;
+  }
+
   public double Getsalario()
   {
     return Salario;
   }
 
-  public Dados(string n, string sn, double s)
+  public void Setsalario(double s)
+  {
+    if(s<=0)
+    {
+      throw new Exception("Salário Invalido"); 
+      
+    }
+
+    else
+    {
+      Salario = s;
+    }
+
+  }
+
+  public Empregado(string n, string sn, double s)
   {
     Nome = n;
     Sobrenome = sn;
     Salario = s;
   }
+
+  public Empregado()
+  {
+    Nome = "Nomade";
+    Sobrenome = "Anonimo";
+    Salario = 1000;
+  }
+
+  
 
 }
--- main.cs
+++ main.cs
@@ -1,8 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class MainClass {
   public static void Main (string[] args) 
   {
+    Empregado anonimo = new Empregado("Nomade", "Anonimo", 1500);
+
+    //Console.WriteLine(anonimo);
+
+    bool chave = true;
+
+    do
+    {
+      try
+      {
+        Console.Write("Digite o Salário: R$" );
+        anonimo.Salario = double.Parse(Console.ReadLine());
+        chave = false;
+      }
+
+      catch
+      {
+        Console.WriteLine("Salário Invalido, tente novamente.");
+        chave = true;
+      }
+    }
+    while(chave);
+    
     
   }
 }
